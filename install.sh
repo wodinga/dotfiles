3f0be42c17613f0a8709f8c6ce2b73a1dc6729a8
David Garcia <david_r_garcia@homedepot.com>
2019-02-07 11:15:56 -0500

    Added some path variables

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-VIMRC="~/.vimrc"
-ZSHRC="~/.zshrc"
+VIMRC="./.vimrc"
+ZSHRC="./.zshrc"
+OHMYZSH=$HOME/.oh-my-zsh
 
+
+
+[[ -f $VIMRC && -f $ZSHRC ]]
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
